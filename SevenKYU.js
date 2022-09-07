01f1e9396887adbe032fa9e8c15f88cd0c651d45
hyusef <mieyle@protonmail.com>
2022-09-07 19:05:56 +0100

    Add getsumOf

--- SevenKYU.js
+++ SevenKYU.js
@@ -127,3 +127,15 @@ function XO(str) {
 
 /* ------------------------------------------------------ */
 
+function getSumOfDigits(integer) {
+  var sum = 0;
+  var digits =  Math.floor(integer).toString();
+
+  for(var ix = 0; ix < digits.length; ix++) {
+    sum += +digits[ix];
+  }
+  return sum;
+} 
+
+
+
