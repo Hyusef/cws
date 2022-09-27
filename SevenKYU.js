c86f3ab5932463c913e6b9e54bd6346078c62efd
hyusef <mieyle@protonmail.com>
2022-09-27 06:53:14 +0100

    predictAge

--- SevenKYU.js
+++ SevenKYU.js
@@ -537,3 +537,13 @@ function giveChange(amount) {
     }
   }
 }
+/* ------------------------------------------------------ */
+function predictAge(age1, age2, age3, age4, age5, age6, age7, age8) {
+  return Math.floor(
+    Math.sqrt(
+      [age1, age2, age3, age4, age5, age6, age7, age8]
+        .map((e) => e * e)
+        .reduce((a, b) => a + b)
+    ) / 2
+  );
+}
