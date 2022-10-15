3d9a64f2524d6bdd8ea5ef01f90bf44c18e7f1ac
hyusef <mieyle@protonmail.com>
2022-10-15 21:15:17 +0100

    Add stringmerge

--- SevenKYU.js
+++ SevenKYU.js
@@ -695,3 +695,6 @@ return code.split('').
 function reverse(string){
   return string.split(' ').reverse().join(' ')
   }
+function stringMerge(string1, string2, letter){
+return string1.slice(0,string1.indexOf(letter))+ string2.slice(string2.indexOf(letter))
+}
