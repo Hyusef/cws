fb70e1ebb24f0deb328fb5982b056e4cccbc67d5
hyusef <mieyle@protonmail.com>
2022-11-26 18:10:00 +0000

    Add OpenOrSenior

--- SevenKYU.js
+++ SevenKYU.js
@@ -777,6 +777,10 @@ function solution(start, finish) {
     }
     return false;
   }
+/*------------------------------------------------------*/
+  function openOrSenior(data){
+    return data.map(e=>e[0]>=55 &&e[1]>7?'Senior':'Open');
+  }
   
   
  
