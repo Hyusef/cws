922860a9d6366455865cf205fca570c379f6db8b
hyusef <mieyle@protonmail.com>
2022-11-29 20:27:21 +0000

    Add solution

--- fiveKyu.js
+++ fiveKyu.js
@@ -2,3 +2,9 @@ function generateHashtag (str) {
 return str.replace(/[ ]{1,}/,'').length== 0 || str.replace(/[ ]{1,}/,'').length >=140   ?false:
 '#'+str.split(/[ ]{1,}/).map(e=>e[0]?.toUpperCase() + e.slice(1)).join('');
 }
+/*------------------------------------------------------*/
+var solution = function(f, s) {
+    let a = []; 
+    for(i in f){a.push(Math.abs(f[i]-s[i])**2);}
+    return a.reduce((a,b)=>a+b)/f.length
+}
